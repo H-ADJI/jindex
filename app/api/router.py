cf64046537f0fedd863230aa52e14207096869bc
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-31 22:06:20 +0100

    initial fiel structure

---
+++ app/api/router.py
@@ -0,0 +1,7 @@
+'''
+File: router.py
+File Created: Tuesday, 31st January 2023 10:02:33 pm
+Author: KHALIL HADJI 
+-----
+Copyright:  KHALIL HADJI 2023
+'''
---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+File: main.py
+File Created: Tuesday, 31st January 2023 10:02:15 pm
+Author: KHALIL HADJI 
+-----
+Copyright:  KHALIL HADJI 2023
+'''
